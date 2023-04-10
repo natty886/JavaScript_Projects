b35f841fe40133c691b938859a2de864c1dbe24e
natty886 <84761201+natty886@users.noreply.github.com>
2023-04-10 16:31:37 -0700

    Basic javascript functions

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//This is my first function display
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("demo") .innerHTML = str;
+}
+
+//This function uses the += operator
+function My_Second_Function() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!";
+    document.getElementById("demo2") .innerHTML = sentence;
+}
