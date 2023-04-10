9c3ad9e15312a9db7a8689647fa3a9517534234f
natty886 <84761201+natty886@users.noreply.github.com>
2023-04-10 15:18:15 -0700

    First javascript assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+
+//created two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string.";
+
+//utilized window alert
+window.alert(Sent1);
+
+//utilized document write and concatenate variables
+document.write(Sent1+Sent2);
+
+//wrote expression
+document.write(" This is the expression"+" 7 x 7 = "+ 7*7);
